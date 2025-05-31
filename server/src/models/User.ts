deeedae826f7b7130f5e5512d3eac2caefcc6e6f
Jerald Whitley, Jr. <jlwj2468@gmail.com>
2025-05-31 07:29:40 -0400

    Update User.ts

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,5 @@
 import { Schema, model, type Document } from 'mongoose';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 
 // import schema from Book.js
 import bookSchema from './Book.js';

a3f3ca803567db4464d7c2ad8d838fcbf7af8952
Jerald Whitley, Jr. <jlwj2468@gmail.com>
2025-05-31 07:38:21 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,11 @@
-// import express from 'express';
-// import path from 'node:path';
-// import db from './config/connection.js';
-// import routes from './routes/index.js';
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
-
 // src/server.ts
-import express from 'express';
 import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 import { ApolloServer } from '@apollo/server';

b7c97611eb5d938d38505743a00f3e65b9dc1eca
Jaybird7828 <whitleje@ad.unc.edu>
2025-05-31 07:11:08 -0400

    finished

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,15 +1,3 @@
-
-// declare namespace Express {
-//   interface Request {
-//     user: {
-//       _id: unknown;
-//       username: string;
-//     };
-//   }
-// }
-
-
-
 import type { JwtPayload } from '../../services/auth';
 
 declare global {

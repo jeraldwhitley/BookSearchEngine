c66cde1bc04f0f186a11e1e5e1fdbc589fd98610
Jaybird7828 <whitleje@ad.unc.edu>
2025-06-01 05:19:44 -0400

    fixed3

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,5 @@
 import { Schema, model, type Document } from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { type IBook, bookSchema } from './Book.js';
 
 interface IUser extends Document {
